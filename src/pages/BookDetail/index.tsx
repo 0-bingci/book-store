0482dff27832b2278a42e70ca19d0dfb69a647f0
陈佳熙 <929702181@qq.com>
2025-11-03 12:33:38 +0800

    feat: 项目初始化

---
+++ src/pages/BookDetail/index.tsx
@@ -0,0 +1,9 @@
+const BookDetail = () => {
+  return (
+    <div>
+      <h1>BookDetail</h1>
+    </div>
+  );
+};
+
+export default BookDetail;
---
+++ src/router/router.tsx
@@ -0,0 +1,23 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import App from '../App';
+import Home from '../pages/Home';
+import BookDetail from '../pages/BookDetail';
+import BookReader from '../pages/BookReader';
+
+// 定义路由规则
+const router = createBrowserRouter([
+  {
+    path: '/', // 根路径
+    element: <App />,
+    children: [
+      { path: 'book', element: <Home /> },
+      { path: 'book-detail', element: <BookDetail /> },
+      { path: 'book-reader', element: <BookReader /> },
+    ],
+  },
+]);
+
+// 导出路由提供者组件
+export default function Routes() {
+  return <RouterProvider router={router} />;
+}
